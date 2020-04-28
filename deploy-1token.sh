30f5d5d430e7cbffbdb7c6acd18b3e5311549b5c
panda-1token <panda@1token.trade>
2020-04-28 15:20:12 +0800

    use hist-quote

--- deploy-1token.sh
+++ deploy-1token.sh
@@ -1,13 +1,13 @@
 set -e
 git checkout guide
 git pull
-git checkout hist-data
+git checkout hist-quote
 git pull
 git checkout gh-pages
 git pull
 
 rm /tmp/docs-guide -rf
-rm /tmp/docs-hist-data -rf
+rm /tmp/docs-hist-quote -rf
 
 git checkout guide
 git pull
@@ -15,17 +15,17 @@ bundle exec middleman build --clean
 cp build /tmp/docs-guide -R
 
 
-git checkout hist-data
+git checkout hist-quote
 git pull
 bundle exec middleman build --clean
-cp build /tmp/docs-hist-data -R
+cp build /tmp/docs-hist-quote -R
 
 
 git checkout gh-pages
-rm hist-data guide -rf
+rm hist-quote guide -rf
 
 cp /tmp/docs-guide guide -R
-cp /tmp/docs-hist-data hist-data -R
+cp /tmp/docs-hist-quote hist-quote -R
 
 git add .
 git commit -a -m "deploy"
